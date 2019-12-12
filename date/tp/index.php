a337e6ff6710db62c174a49a8e5ae5af9f423160
LoicNGR <pro.nogierloic@gmail.com>
2019-12-12 16:19:15 +0100

    add tp date

---
+++ date/tp/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>tp</title>
+
+    <style>
+        body {
+            background: teal;
+            color: white;
+            font-size: 1.5rem;
+        }
+    </style>
+</head>
+<body>
+    <form action="" method="post">
+        <select name="listeMois" id="">
+            <option value="janvier">janvier</option>
+        </select>
+        <select name="listeAnnee" id="">
+            <option value="2019">2019</option>
+        </select>
+        <button type="submit">Envoyer</button>
+    </form>
+</body>
+</html>
